7a3d0f82565dff72caf57acd7249aa7d54738495
Annelie <annelie.j.rova@gmail.com>
2024-04-20 22:11:25 +0200

    Courses

--- WebApi/Controllers/CoursesController.cs
+++ WebApi/Controllers/CoursesController.cs
@@ -1,14 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Infrastructure.Contexts;
+using Infrastructure.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace WebApi.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class CoursesController : ControllerBase
+public class CoursesController(ApiContext context) : ControllerBase
 {
+    private readonly ApiContext _context = context;
+
     [HttpGet]
     public async Task<IActionResult> GetAll()
     {
-        return Ok();
+        var courses = await _context.Courses.ToListAsync();
+
+        return Ok(courses);
     }
+
+    
 }
